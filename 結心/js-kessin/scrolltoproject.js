2226f5fcbd252b0cdcb52499ca93b329da41e3d0
Yoshinobu Shibata <>
2020-07-13 10:15:01 +0900

    no message

--- 結心/js-kessin/scrolltoproject.js
+++
@@ -1,7 +0,0 @@
-function scrollToProject(){
-    headerHeight = $("#header").innerHeight(); 
-    var element = document.getElementById('project'); // 移動させたい位置の要素を取得
-    var position = $(element).offset().top - headerHeight - 10;    // 一番上からの位置を取得
-    $('html, body').animate({scrollTop:position}, 800, 'swing');
-  	scrollTo(0, position);
-}
