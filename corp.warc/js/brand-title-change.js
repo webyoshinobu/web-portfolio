5a1744652893efb5f994478a970fe1b3dd01ac9e
Yoshinobu Shibata <>
2020-07-13 10:03:21 +0900

    no message

--- corp.warc/js/brand-title-change.js
+++
@@ -1,9 +0,0 @@
-const txts = $('.text');
-let txtIndex = -1;
-txts.hide()
-
-function showNextTxt() {
-    txtIndex++;
-    txts.eq(txtIndex % txts.length).fadeIn(2000).delay(3000).fadeOut(2000, showNextTxt);
-  }
-  showNextTxt();
