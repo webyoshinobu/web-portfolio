3ec514243e416f34cbc0d08006379b439984fa74
webyoshinobu <49665799+webyoshinobu@users.noreply.github.com>
2020-10-07 18:35:13 +0900

    Add files via upload

---
+++ jsホバーテンプレ/script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  // 言語一覧
+
+  $('.lesson').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+
+});
---
+++ jsモーダルテンプレ/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  // 新規登録モーダル
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+
+});
